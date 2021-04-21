70fb796283d4315205e643d9e2645fb8448bc104
renhuiyao <853660920@qq.com>
2021-04-21 05:05:28 -0700

    buddy_system first time

--- lab2/kern/mm/pmm.c
+++ lab2/kern/mm/pmm.c
@@ -8,6 +8,7 @@
 #include <default_pmm.h>
 #include <sync.h>
 #include <error.h>
+#include<buddy_system.h>
 
 /* *
  * Task State Segment:
@@ -137,7 +138,8 @@ gdt_init(void) {
 //init_pmm_manager - initialize a pmm_manager instance
 static void
 init_pmm_manager(void) {
-    pmm_manager = &default_pmm_manager;
+    //pmm_manager = &default_pmm_manager;
+    pmm_manager = &buddy_system;
     cprintf("memory management: %s\n", pmm_manager->name);
     pmm_manager->init();
 }
---
+++ lab2/obj/kern/mm/buddy_system.d
@@ -0,0 +1,4 @@
+obj/kern/mm/buddy_system.o obj/kern/mm/buddy_system.d: \
+ kern/mm/buddy_system.c libs/list.h libs/defs.h kern/mm/pmm.h \
+ kern/mm/mmu.h kern/mm/memlayout.h libs/atomic.h kern/debug/assert.h \
+ kern/mm/buddy_system.h
